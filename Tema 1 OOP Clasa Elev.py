39814d2eab7e69997d5d39bb824f3910c6f9371f
JoanaP16 <perciun.ioana@yahoo.com>
2023-08-14 06:21:25 +0300

    Completed with classmethod

--- Tema 1 OOP Clasa Elev.py
+++ Tema 1 OOP Clasa Elev.py
@@ -1,18 +1,18 @@
 class Student:
     student_catalog = {}
 
-    def __init__ (self, name, surname):
+    def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.catalog = []
 
-    def get_marks (self, mark):
+    def get_marks(self, mark):
         self.mark = mark
         self.catalog.append(mark)
         Student.student_catalog[self.name] = self.catalog
 
     def student_average(self):
-        student_average = float(sum(self.catalog)) / len(self.catalog)
+        return float(sum(self.catalog)) / len(self.catalog)  # daca pun return e mai usor de folosit in afara functiei
 
     def overall_average(self):
         sumac = 0  # suma tuturor notelor
@@ -21,13 +21,30 @@ class Student:
             for valoare in Student.student_catalog[i]:
                 sumac += valoare
                 count += 1
-        general_average = sumac / count
+        return sumac / count
 
     def print_detailes(self):
-        print(f'Student {self.name}-{self.surname} get average {student_average}. General average is {general_average}')
+        student_avg = self.student_average()
+        overall_avg = self.overall_average()
+        detailes = f'Student {self.name}-{self.surname} get average {student_avg}. General average is {overall_avg}\n'
 
-        with open('student_average.txt', 'a') as file:
-            file.write(f' Student {self.name}-{self.surname} has average {self.student_average}',)
+        with open('student_average.txt', 'r') as file:
+            lines = file.readlines()
+            if detailes not in lines:
+                print(detailes)
+                with open('student_average.txt', 'a') as to_add:
+                    to_add.write(detailes)
+
+    @classmethod
+    def get_file(cls, file):
+        students = []
+        with open(file, 'r') as file_class:
+            lines = file.readlines()
+            for line in lines:
+                name, surname = line.strip().split()
+                student = cls(name, surname)
+                students.append(student)
+        return students
 
 
 Popescu_Melania = Student('Popescu', 'Melania')
@@ -37,3 +54,8 @@ Manole_Denisa.get_marks(100)
 Popescu_Melania.get_marks(90)
 Manole_Denisa.get_marks(75)
 Manole_Denisa.student_average()
+Popescu_Melania.print_detailes()
+Manole_Denisa.print_detailes()
+Andreescu_Anemona = Student('Andreescu', 'Anemona')
+
+students_get_file = Student.get_file('student_average.txt')
